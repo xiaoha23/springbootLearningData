82dbffb3a5e17ecc191f7f84e2ffc430ebbf7e1e
Xiaonan Wei <xiaonanwei8423@gmail.com>
2019-02-24 18:15:52 -0400

    after tutorial 29

---
+++ src/main/java/com/xiaonan/springbootLearning/courseapidata/topic/TopicRepository.java
@@ -0,0 +1,7 @@
+package com.xiaonan.springbootLearning.courseapidata.topic;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface TopicRepository extends CrudRepository<Topic, String>{
+
+}
---
+++ src/test/java/com/xiaonan/springbootLearningcom/example/democourseapidata/DemocourseApiDataApplicationTests.java
@@ -0,0 +1,16 @@
+package com.xiaonan.springbootLearningcom.example.democourseapidata;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DemocourseApiDataApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
